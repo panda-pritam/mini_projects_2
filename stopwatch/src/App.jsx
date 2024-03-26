fa43549ab1853590008c25bfb60d864d32bd0905
panda-pritam <mondalrumki02@gmail.com>
2024-03-27 04:05:02 +0530

    stop watch coded

---
+++ stopwatch/src/App.jsx
@@ -0,0 +1,56 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+function App() {
+  let [isStarted, setIsStarted] = useState(false);
+  let [time, setTime] = useState(0);
+  let [str, setStr] = useState("0:00");
+  let handleStartStop = () => {
+    setIsStarted(!isStarted);
+  };
+  useEffect(() => {
+    let timerId = 0;
+    if (isStarted) {
+      timerId = setInterval(() => {
+        setTime((prev) => {
+          return prev + 1;
+        });
+      }, 1000);
+      return () => {
+        if (timerId) {
+          clearInterval(timerId);
+        }
+      };
+    }
+  }, [isStarted]);
+  useEffect(() => {
+    if (time > 0) {
+      let min = Math.floor(time / 60);
+      let sec = time % 60;
+      if (sec < 10) {
+        sec = "0" + sec;
+      }
+      console.log(`${min}:${sec}`);
+      setStr((prev) => `${min}:${sec}`);
+    }
+  }, [time]);
+  let handleReset = () => {
+    setTime(0);
+    setStr("0:00");
+    setIsStarted(false);
+  };
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <p>Time: {str}</p>
+      <button type="button" onClick={handleStartStop}>
+        Start
+      </button>
+      <button type="button" onClick={handleReset}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default App;
